be00b49a3775d8265fd2119d335fe220a0f647d9
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2023-12-28 18:57:07 +0100

    basic opcodes implemented

--- 10_emulator/include/cpu.h
+++ 10_emulator/include/cpu.h
@@ -12,22 +12,22 @@
 #if CPU_CURRENT == CPU_6502
 #define cpu_current_lexer_next_token(input, line, pos, err)     cpu_6502_lexer_next_token(input, line, pos, err)
 #define cpu_current_interpret_instruction(node, cpu, err)       cpu_6502_interpret_instruction(node, cpu, err)
-#define cpu_current_parser_cst_node(input_arr, i, err)          cpu_6502_parser_cst_node(input_arr, i, err)
+#define cpu_current_parser_next_token(input_arr, i, err)        cpu_6502_parser_next_token(input_arr, i, err)
 
 #elif CPU_CURRENT == CPU_8086
 #define cpu_current_lexer_next_token(input, line, pos, err)     cpu_8086_lexer_next_token(input, line, pos, err)
 #define cpu_current_interpret_instruction(node, cpu, err)       cpu_8086_interpret_instruction(node, cpu, err)
-#define cpu_current_parser_cst_node(input_arr, i, err)          cpu_8086_parser_cst_node(input_arr, i, err)
+#define cpu_current_parser_next_token(input_arr, i, err)        cpu_8086_parser_next_token(input_arr, i, err)
 
 #elif CPU_CURRENT == CPU_386
 #define cpu_current_lexer_next_token(input, line, pos, err)     cpu_386_lexer_next_token(input, line, pos, err)
 #define cpu_current_interpret_instruction(node, cpu, err)       cpu_386_interpret_instruction(node, cpu, err)
-#define cpu_current_parser_cst_node(input_arr, i, err)          cpu_386_parser_cst_node(input_arr, i, err)
+#define cpu_current_parser_next_token(input_arr, i, err)        cpu_386_parser_next_token(input_arr, i, err)
 
 #else
 #define cpu_current_lexer_next_token(input, line, pos, err)     cpu_unknown_lexer_next_token(input, line, pos, err)
 #define cpu_current_interpret_instruction(node, cpu, err)       cpu_unknown_interpret_instruction(node, cpu, err)
-#define cpu_current_parser_cst_node(input_arr, i, err)          cpu_unknown_parser_cst_node(input_arr, i, err)
+#define cpu_current_parser_next_token(input_arr, i, err)        cpu_unknown_parser_next_token(input_arr, i, err)
 
 #endif
 
